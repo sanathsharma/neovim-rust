dab98d264df889686776f827129849484070ac3e
Sanath Sharma <48090208+sanathsharma@users.noreply.github.com>
2024-03-05 20:26:35 +0000

    add mappings
    - tmux vim inter navigation
    - bind jk to esc

--- mappings.lua
+++ mappings.lua
@@ -27,4 +27,19 @@ M.crates = {
   }
 }
 
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window Left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+  }
+}
+
+M.vim = {
+  i = {
+    ["jk"] = { "<ESC>", "escape insert mode" , opts = { nowait = true }},
+  }
+}
+
 return M
--- plugins.lua
+++ plugins.lua
@@ -2,6 +2,10 @@ local cmp = require "cmp"
 
 local plugins = {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
